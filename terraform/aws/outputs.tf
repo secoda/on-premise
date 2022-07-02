acc71c954fe1cc06aca9d3f16ad341b3f0cc2c3d
Melker Öhrman <101633418+mohrman@users.noreply.github.com>
2022-07-02 23:45:38 +0200

    feat: adding sec group id as module output (#2)

--- terraform/aws/outputs.tf
+++ terraform/aws/outputs.tf
@@ -2,3 +2,8 @@ output "aws_lb_dns" {
   description = "The load balancer url. This will start returning HTTP 200 approx. 5 minutes after the terraform is deployed."
   value       = module.ecs.aws_lb_dns
 }
+
+output "security_group_id" {
+  description = "ECS security group ID used for Secoda"
+  value       = aws_security_group.ecs_sg.id
+}
