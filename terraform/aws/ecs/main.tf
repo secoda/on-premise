5204ab973ad67eb74ffeae3e304fdf73d40ab46e
Carter Sprigings <carter@blacklithium.com>
2022-07-04 10:38:33 -0400

    feat: change to ELBSecurityPolicy-FS-1-2-Res-2020-10 for the load balancer

--- terraform/aws/ecs/main.tf
+++ terraform/aws/ecs/main.tf
@@ -138,8 +138,9 @@ resource "aws_lb_listener" "https" {
   load_balancer_arn = aws_lb.main[0].id
   certificate_arn   = var.certificate_arn
 
-  port     = 443
-  protocol = "HTTPS"
+  port       = 443
+  protocol   = "HTTPS"
+  ssl_policy = "ELBSecurityPolicy-FS-1-2-Res-2020-10"
 
   default_action {
     target_group_arn = aws_lb_target_group.https[0].id
